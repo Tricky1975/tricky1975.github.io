b629f27b0140ade9516431fce6524106578e25ce
Tricky <rachel.tricky@hotmail.com>
2017-03-31 00:43:43 +0200

    Update in Windows

---
+++ wingitpush.bat
@@ -0,0 +1,4 @@
+"C:\program files\git\bin\git" add -A > "C:\Users\Rachel\AppData\Roaming\Phantasar Productions\DevLog\SwapGitResult.txt"
+"C:\program files\git\Bin\git" commit -m "Update in Windows"
+"C:\program files\git\bin\git" push 
+pause
